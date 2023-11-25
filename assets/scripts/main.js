e0d761427c2174fd54fb6b63a492e3d9c935a6da
fabiopc <fabiopc@debian-BULLSEYE-live-builder-AMD64>
2023-11-25 12:59:28 -0400

    tela main feita com responsivo e trocar de cor

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,3 +23,34 @@ new Chart(chart, {
         responsive: true
     }
 });
+
+
+// show or hide sidebar
+
+const menuBtn = document.querySelector('#menu-btn')
+const closeBtn = document.querySelector('#close-btn')
+const sidebar = document.querySelector('aside')
+
+menuBtn.addEventListener('click',() => {
+    // Verificar se o sidebar está visível
+    const isSidebarVisible = sidebar.style.display === 'block';
+
+    // Alternar a visibilidade do sidebar com base no estado atual
+    sidebar.style.display = isSidebarVisible ? 'none' : 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+})
+
+
+// change theme
+const themeBtn = document.querySelector('.theme-btn')
+
+themeBtn.addEventListener('click',()=> {
+    document.body.classList.toggle('dark-theme');
+    
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
